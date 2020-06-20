25292731a6cf6288b6e198124868e3fe3d91a1df
TigerOnline <53658717+TigerOnline@users.noreply.github.com>
2020-06-19 17:54:10 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 client = commands.Bot(command_prefix = '!')
 @client.command()
 async def message(ctx, arg):
-    channel = client.get_channel('A channel id')
+    channel = client.get_channel('A channel id') #Channel id's datatype is an int, not a string!
     await channel.send(arg)
 
 client.run('Haha no')
